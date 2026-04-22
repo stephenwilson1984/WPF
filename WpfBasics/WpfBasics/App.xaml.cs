24513fad3a4434c35ceb84b4d1d803c56e90e7ac
Stephen Wilson <stephenwilson1984@outlook.com>
2026-04-22 16:50:59 +0100

    Checkpoint

--- WpfBasics/WpfBasics/App.xaml.cs
+++ WpfBasics/WpfBasics/App.xaml.cs
@@ -1,14 +1,7 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
 
-namespace WpfBasics
+namespace WpfBasics;
+
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
-
 }
