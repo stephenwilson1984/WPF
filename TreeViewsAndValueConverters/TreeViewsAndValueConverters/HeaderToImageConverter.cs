1df2ea45741c582016e16ef607e540b826c6bca8
Stephen Wilson <stephenwilson1984@outlook.com>
2026-05-14 17:18:27 +0100

    Complete

---
+++ TreeViewsAndValueConverters/TreeViewsAndValueConverters/HeaderToImageConverter.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace TreeViewsAndValueConverters;
+
+[ValueConversion(typeof(string), typeof(BitmapImage))]
+public class HeaderToImageConverter : IValueConverter
+{
+    public static HeaderToImageConverter Instance = new HeaderToImageConverter();
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        // If the path is null, ignore
+        if (value is not string path)
+        {
+            return null;
+        }
+
+        // Get the name of the file or folder
+        var name = MainWindow.GetFileOrFolderName(path);
+
+        // By default, we assume file
+        var image = "Images/file.png";
+
+        // If the name is blank, we presume it's a drive as we cannot have a blank file or folder name
+        if (string.IsNullOrEmpty(name))
+        {
+            image = "Images/drive.png";
+        }
+        else if (new FileInfo(path).Attributes.HasFlag(FileAttributes.Directory))
+        {
+            image = "Images/folder-closed.png";
+        }
+
+        return new BitmapImage(new Uri($"pack://application:,,,/{image}"));
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
